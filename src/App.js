d5f570b067547dbbeb43c59490e41c53b7454561
teataeho <rlaxogh9504@gmail.com>
2023-06-12 15:49:03 +0900

    2023.06.12 이벤트와 스타일

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import SayHello from './SayHello';
-import Hello from './Hello';
+import SubmitEvent from './component/chap01_event/SubmitEvent';
 
 //리액트에서는 하나의 컴포넌트가 여러개의 엘리먼트를 반환할 수 있습니다.
 //JSX를 작성할 때 return 문 안에 반드시 하나의 최상위 태그가 있어야 합니다.
@@ -21,7 +20,7 @@ function App() {
   //{} 안에서 함수의 호출문이나 변수참조는 가능.
   return (
     <>
-      { looping() }
+      <SubmitEvent />
     </>
   );
 }
---
+++ src/component/chap01_event/ClickEvent.js
@@ -0,0 +1,28 @@
+//rafce
+import React from 'react'
+
+const ClickEvent = () => {
+
+  const greeting = e => {
+    alert('안뇽안뇽~ ㅋㅋㅋㅋ')
+  }
+
+  const changeBox = e => {
+    const $box = document.querySelector('.box');
+    $box.style.background = 'skyblue';
+    $box.style.width = '200px';
+    $box.style.height = '200px';
+  }
+
+  return (
+    <>
+        <button id='btn1' onClick={ () => alert('클릭잘했어~') }>클릭해봐~</button>
+        <button id='btn2' onClick={ greeting }>클릭2</button>
+        <button id='btn3' onMouseOver={ changeBox }>클릭3</button>
+
+        <div className='box'></div>
+    </>
+  )
+}
+
+export default ClickEvent
